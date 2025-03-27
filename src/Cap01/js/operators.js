7524701ce692f3264fc4d75de56e202e932f8eb0
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-03-27 15:03:55 -0300

    Add operators examples and update HTML structure

---
+++ src/Cap01/js/operators.js
@@ -0,0 +1,32 @@
+var num = 0;
+console.log(num);
+
+num = num + 2;
+console.log(num);
+
+num = num * 3;
+console.log(num);
+
+num = num / 2;
+console.log(num);
+
+num++;
+console.log(num);
+
+num--;
+console.log(num);
+
+num += 1;
+console.log(num);
+
+num -= 2;
+console.log(num);
+
+num *= 3;
+console.log(num);
+
+num /= 2;
+console.log(num);
+
+num %= 3;
+console.log(num);
