42c3300949af48d24742895bd58e91fd11ec9e27
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-03-27 18:10:23 -0300

    Add functions and loops examples to HTML and create functions.js and loops.js for demonstration

---
+++ src/Cap01/js/functions.js
@@ -0,0 +1,18 @@
+function sayHello(){
+    console.log('Hello!');
+}
+
+sayHello(); // Hello!
+
+function output(text){
+    console.log(text);
+}
+
+output('Hello!'); // Hello!
+
+function sum(a, b){
+    return a + b;
+}
+
+var result = sum(1, 2);
+output(result); // 3
---
+++ src/Cap01/js/loops.js
@@ -0,0 +1,18 @@
+console.log('for...')
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log('while...')
+var i = 0;
+while( i< 10){
+    console.log(i);
+    i++;
+}
+
+console.log('do...while...')
+var i = 0;
+do{
+    console.log(i);
+    i++;
+}while(i<10)
