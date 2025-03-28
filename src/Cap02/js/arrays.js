9f0c7a51dd4c58c351796ce0da22b4868a697965
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-03-28 13:04:44 -0300

    Enhance array examples with initialization, iteration, and Fibonacci sequence

--- src/Cap02/js/arrays.js
+++ src/Cap02/js/arrays.js
@@ -6,8 +6,23 @@ averageTemp[2] = 32;
 averageTemp[3] = 40.1;
 console.log(averageTemp);
 
-console.log('>>> Como declarar um array?')
+console.log('>>> Criando e inicializando arrays')
 let daysOfWeek = new Array();
 daysOfWeek = new Array(7);
 daysOfWeek = new Array('Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado');
 console.log(daysOfWeek);
+console.log(daysOfWeek.length);
+
+console.log('Acessando elementos e iterando em um array');
+for (var i = 0; i < daysOfWeek.length; i++){
+    console.log(daysOfWeek[i]);
+}
+
+let fibonacci = [];
+fibonacci[1] = 1;
+fibonacci[2] = 1;
+for (var i = 3; i <= 200; i++){
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+console.log(fibonacci);
+console.log(fibonacci[fibonacci.length-1]);
