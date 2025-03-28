7e51cc44543141e4550e09a5b0992ac3103127a6
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-03-28 11:40:46 -0300

    Add examples of sum function with default and rest parameters in other-functions.js

--- src/Cap01/js/other-functions.js
+++ src/Cap01/js/other-functions.js
@@ -65,20 +65,36 @@ let area = circleArea3(40);
 console.log(`A área do circulo com raio ${raio} é igual a ${area}.`);
 
 console.log('>>> Valores Padrões');
-function sum(a , b){
+function sum(a , b , c){
     if (a === undefined){
         a = 0;
     }
     if (b === undefined){
         b = 0;
     }
+    if (c === undefined){
+        c = 0;
+    }
 
-    return a + b;
+    return a + b + c;
 }
 
-function sumDefault( a = 0, b = 0){
-    return a + b;
+function sumDefault( a = 0, b = 0, c = 0){
+    return a + b + c;
 }
 
 console.log(`Sem valor padrão:: ${sum(1)}`);
 console.log(`Com valor padrão:: ${sumDefault()}`);
+
+console.log('>>> Spread e Rest');
+let params = [1, 2, 3];
+let result = sumDefault(...params);
+console.log(`Resultado: ${result}`);
+
+//soma x + y e multiplica pelo tamanho do array a
+function restParameterFunction(x, y, ...a){
+    return (x + y) * a.length
+}
+var restParameterFunctionResult = restParameterFunction(1, 2, 'Hello', true, 7);
+console.log(restParameterFunctionResult);
+
