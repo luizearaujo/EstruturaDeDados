6a0d807c3db17fa8bb9f5f40c4b8c8db688ee5ed
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-03-28 12:39:51 -0300

    Add HTML structure and JavaScript examples for arrays

---
+++ src/Cap02/js/arrays.js
@@ -0,0 +1,13 @@
+console.log('>>> O que é um array?')
+var averageTemp = []
+averageTemp[0] = 39;
+averageTemp[1] = 36.8;
+averageTemp[2] = 32;
+averageTemp[3] = 40.1;
+console.log(averageTemp);
+
+console.log('>>> Como declarar um array?')
+let daysOfWeek = new Array();
+daysOfWeek = new Array(7);
+daysOfWeek = new Array('Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado');
+console.log(daysOfWeek);
