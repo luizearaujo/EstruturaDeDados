6e26867226d0f550b28297f6606637514eea4994
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2025-04-25 11:10:32 -0300

    criação da estrutura base do capitulo 3 e da classe para representar a pilha

---
+++ src/Cap03/js/stacks.js
@@ -0,0 +1,39 @@
+console.log('>>> Capitulo 3 - Pilhas');
+
+function Stack(){
+    
+    //Adiciona um novo item (ou vários itens) ao topo da pilha
+    function push(e){
+
+    }
+
+    //Remove o item do topo da pilha. Também devolfeo elemento removido
+    function pop(){
+
+    }
+
+    //Devolve o elemento que está no topo da pilha. Esta não é modificada
+    //(o elemento não é removido; ele é devolvido apenas como informação)
+    function peek(){
+
+    }
+
+    //Devolve true se a pilha não contiver nenhum elemento e false se o 
+    //tamanho da pilha for maior que 0
+    function isEmpty(){
+
+    }
+
+    //Remove todos os elementos da pilha
+    function clear(){
+
+    }
+
+    //Devolve o número de elementos contidos na pilha. É semelhante à 
+    //propriedade lenght de um array
+    function size(){
+
+    }
+}
+
+let items = [];
