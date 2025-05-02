94c76a9fd6ee2df58c6310865a29ac4a026e020e
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2025-05-02 14:05:20 -0300

    Refatoração da classe stack com WeakMap para tornar a pilha privada

--- src/Cap03/js/stacks-es6-weakmap.js
+++ src/Cap03/js/stacks-es6-weakmap.js
@@ -1,46 +1,55 @@
-const items = new WeakMap();
+let Stack = (function () {
 
-class Stack{
-    constructor(){
-        items.set(this, []);
+    const items = new WeakMap();
+
+    class Stack {
+
+        constructor() {
+            items.set(this, []);
+        }
+
+        push(element) {
+            let s = items.get(this);
+            s.push(element);
+        }
+
+        pop() {
+            let s = items.get(this);
+            let r = s.pop();
+            return r;
+        }
+
+        peek() {
+            let s = items.get(this);
+            return s[s.length - 1];
+        }
+
+        isEmpty() {
+            let s = items.get(this);
+            return s.length == 0;
+        }
+
+        clear() {
+            let s = items.get(this);
+            s = [];
+        }
+
+        size() {
+            let s = items.get(this);
+            return s.length;
+        }
+
+        print() {
+            let s = items.get(this);
+            console.log(s.toString());
+        }
     }
 
-    push(element){
-        let s = items.get(this);
-        s.push(element);
-    }
+    return Stack;
 
-    pop(){
-        let s = items.get(this);
-        let r = s.pop();
-        return r;
-    }
+})();
 
-    peek(){
-        let s = items.get(this);
-        return s[s.length - 1];
-    }
 
-    isEmpty(){
-        let s = items.get(this);
-        return s.length == 0;
-    }
-
-    clear(){
-        let s = items.get(this);
-        s = [];
-    }
-
-    size(){
-        let s = items.get(this);
-        return s.length;
-    }
-
-    print(){
-        let s = items.get(this);
-        console.log(s.toString());
-    }
-}
 
 let stack = new Stack();
 console.log('stack.isEmpty: ' + stack.isEmpty());
