331b0fa47b6ab4bf7aa7561dc90d035d9d8f0d1f
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2025-05-09 11:14:41 -0300

    Implementação inicial da classe linked list em ES6 / Implementação da função append

---
+++ src/Cap05/linked-list.js
@@ -0,0 +1,54 @@
+let LinkedList = (function(){
+    
+    let Node = function(element){
+        this.element = element;
+        this.next = null;
+    }
+
+    let legth = 0;
+    let head = null;
+
+    class LinkedList{
+
+        append(element){
+
+        }
+
+        insert(position, element){
+
+        }
+
+        removeAt(element){
+
+        }
+
+        remove(element){
+
+        }
+
+        indexOf(element){
+
+        }
+
+        isEmpty(){
+
+        }
+
+        size(){
+
+        }
+
+        toString(){
+
+        }
+
+        print(){
+
+        }
+
+    }
+
+    return LinkedList;
+
+    
+});
