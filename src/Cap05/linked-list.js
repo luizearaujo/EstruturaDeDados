7bb1d506b287525e242960be826641fd8f351eef
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2025-05-09 11:31:12 -0300

    Correção do nome da propriedade length /Alteração da estrutura da clase Node

--- src/Cap05/linked-list.js
+++ src/Cap05/linked-list.js
@@ -1,17 +1,37 @@
 let LinkedList = (function(){
     
-    let Node = function(element){
-        this.element = element;
-        this.next = null;
+    class Node{
+        constructor(element){
+            this.element = element;
+            this.next = next;
+        }
     }
-
-    let legth = 0;
-    let head = null;
-
+    
     class LinkedList{
+
+        constructor(){
+            this.length = 0;
+            this.head = null;
+        }
 
         append(element){
 
+            let node = new Node(element),
+            current;
+
+            if (head == null){
+                head = node;
+            }else{
+                current = head;
+
+                while(current.next){
+                    current = current.next;
+                }
+
+                current.next = node;
+            }
+
+            length++;
         }
 
         insert(position, element){
@@ -52,3 +72,9 @@ let LinkedList = (function(){
 
     
 });
+
+let list = new LinkedList();
+list.append(15);
+list.append(8);
+
+console.log(list);
