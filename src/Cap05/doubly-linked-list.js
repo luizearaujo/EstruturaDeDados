71d6e43db10f893fa8ca8f491e8ab844dd0f5fb8
Luiz Eduardo Dario de Araujo <luiz.earaujo@gmail.com>
2025-05-10 11:25:22 -0300

    feat: add initial doubly linked list structure

---
+++ src/Cap05/doubly-linked-list.js
@@ -0,0 +1,55 @@
+let DoublyLinkedList = (function(){
+
+    class Node{
+        constructor(element){
+            this.element = element;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    class DoublyLinkedList{
+
+        constructor(){
+            this.length = 0;
+            this.head = null;
+            this.tail = null;
+        }
+
+        append(element){
+
+        }
+
+        insert(position, element){
+
+        }
+
+        removeAt(position){
+
+        }
+
+        remove(element){
+
+        }
+
+        indexOf(element){
+
+        }
+
+        isEmpty(){
+
+        }
+
+        size(){
+
+        }
+
+        toString(){
+            
+        }
+
+    }
+
+    return DoublyLinkedList;
+
+})();
